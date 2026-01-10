c13f56cc70fe3dcfd90b1e0d3f16002e0116ee03
Denis Molin <molind@inbox.ru>
2026-01-10 20:59:27 +0700

    Add /api/, .env

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, status, Request
+from fastapi import FastAPI, Depends, HTTPException, status, Request, APIRouter
 from fastapi.responses import RedirectResponse, JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.middleware.cors import CORSMiddleware
@@ -7,7 +7,11 @@ from fastapi.exceptions import RequestValidationError
 import crud, schemas
 from core.database import get_db_session
 
-app = FastAPI(title="URL Shortener")
+app = FastAPI(
+    title="URL Shortener",
+    version="1.0.0",
+    openapi_url="/api/openapi.json",
+    docs_url="/api/docs")
 
 #Настрока CORS
 origins = [
@@ -22,6 +26,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+router = APIRouter(prefix="/api")
 
 
 @app.exception_handler(RequestValidationError)
@@ -47,13 +53,13 @@ async def valedation_exception_handler(request: Request, exc: RequestValidationE
 
 
 #Эндпоинт для созжания коротого URL
-@app.post(path="/shorten", response_model=schemas.URLInfo)
+@router.post(path="/shorten", response_model=schemas.URLInfo)
 async def create_url(url: schemas.URLCreate, db: AsyncSession = Depends(get_db_session)):
     return await crud.create_bd_urls(db=db, url=url)
 
 
 #Эндпоинт для редиректа
-@app.get(path="/{url_key}")
+@router.get(path="/{url_key}")
 async def forward_to_target_url(url_key: str, db: AsyncSession = Depends(get_db_session)):
     db_url = await crud.get_db_url_by_key(db=db, url_key=url_key)
 
@@ -66,10 +72,13 @@ async def forward_to_target_url(url_key: str, db: AsyncSession = Depends(get_db_
 
 
 #"Cтатистика"
-@app.get(path="/stats/{url_key}")
+@router.get(path="/stats/{url_key}")
 async def get_url_status(url_key: str, db: AsyncSession = Depends(get_db_session)):
     db_url = await crud.get_db_url_by_key(db=db, url_key=url_key)
 
     if db_url:
         return db_url
     raise HTTPException(status_code=404, detail="URL not found")
+
+
+app.include_router(router=router)
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -3,7 +3,7 @@ import pytest_asyncio
 
 async def test_create_url(client):
     payload = {"target_url": "https://python.org"}
-    response = await client.post("/shorten", json=payload)
+    response = await client.post("/api/shorten", json=payload)
 
     if response.status_code == 422:
         print(response.json())
@@ -17,10 +17,10 @@ async def test_create_url(client):
 
 async def test_redirect_url(client):
     payload = {"target_url": "https://fastapi.tiangolo.com"}
-    res_create = await client.post("/shorten", json=payload)
+    res_create = await client.post("/api/shorten", json=payload)
     key = res_create.json()["short_key"]
 
-    response = await client.get(f"/{key}")
+    response = await client.get(f"/api/{key}")
 
     assert response.status_code == 307
     assert response.headers["location"] == "https://fastapi.tiangolo.com/"
@@ -30,12 +30,12 @@ async def test_duplicate_url(client):
     #тесть на то, что для одной ссылки возвращается один и тот же ключ
     payload = {"target_url": "https://google.com"}
 
-    res1 = await client.post("/shorten", json=payload)
-    res2 = await client.post("/shorten", json=payload)
+    res1 = await client.post("/api/shorten", json=payload)
+    res2 = await client.post("/api/shorten", json=payload)
     
     assert res1.json()["short_key"] == res2.json()["short_key"]
 
 
 async def test_not_found_url(client):
-    response = await client.get("/nonexistent")
+    response = await client.get("/api/nonexistent")
     assert response.status_code == 404
